6c104072f514f49ec75a792ec3d14ebaa83029c8
Robert Healy <rhealy@coursera.org>
2020-10-27 22:01:12 -0400

    wipBeforeUpdate

--- action/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var src_1 = require("./src");
-src_1.main();
--- action/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { getInput, setOutput, setFailed } from '@actions/core';
-
-export const main = async () => {
-    const name = getInput('NAME');
-
-    console.log("Got name " + name);
-
-    setOutput('name', name);
-};
